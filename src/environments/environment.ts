d07fe4b075aa616a51ce3ad012d0ab4b33f81ea6
Andriy Golovko <agolovko@Andriys-MBP-2.fritz.box>
2026-07-08 10:49:41 +0200

    Proxy UI API to backend service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:4000/api',
+  apiUrl: '/api',
   googleClientId: '137179614846-6g2eervv7hmmj0p2fofhmiv761oomc4q.apps.googleusercontent.com',
 };
